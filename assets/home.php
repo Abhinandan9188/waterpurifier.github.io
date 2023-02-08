1098bdcccd44550d62416b742d59214531b26618
Abhinandan9188 <121888721+Abhinandan9188@users.noreply.github.com>
2023-02-08 15:33:27 +0530

    this our project

---
+++ assets/home.php
@@ -0,0 +1,416 @@
+<?php get_header() ?>
+
+     <!-- section 1 -->
+      <div class="lg:h-screen flex items-center custom-1">
+         <div class="container mx-auto px-6 lg:px-32 ">
+            <div class="grid items-center md:grid-cols-3 gap-8">
+
+               <div >
+                  <img class="inline" src="assets/drinkomain.png" alt="alternative" />
+               </div>
+               <div class="">
+                  <h1 class="lg:text-3xl text-xl mb-5 font-black text-customcolor">Live better with pure water, <br>
+                     get Drinkon24 
+                  </h1>
+                  <p class="text-lg">Water purifiers installed now!</p>
+               </div>
+               <div>
+                  <div class="block p-6 rounded-lg bg-white ">
+                     <form>
+                        <h3 class="text-center text-customcolor font-normal text-lg">Signup for free Demo</h3>
+                        <div class="form-group mt-6 mb-6">
+                           <input type="email" class="form-control
+                              block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-900
+                              bg-gray-100 bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2"
+                              aria-describedby="emailHelp" placeholder="Name">
+                        </div>
+                        <div class="form-group mt-6 mb-6">
+                           <input type="email" class="form-control
+                              block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-900
+                              bg-gray-100 bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2"
+                              aria-describedby="emailHelp" placeholder="Email">
+                        </div>
+                        <div class="form-group mt-6 mb-6">
+                           <input type="email" class="form-control
+                              block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-900
+                              bg-gray-100 bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2"
+                              aria-describedby="emailHelp" placeholder="Phone No">
+                        </div>
+                        <div class="form-group mt-6 mb-6">
+                           <input type="email" class="form-control
+                              block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-900
+                              bg-gray-100 bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2"
+                              aria-describedby="emailHelp" placeholder="Address">
+                        </div>
+                        <div class="form-group mt-6 mb-6">
+                           <input type="email" class="form-control
+                              block
+                              w-full
+                              px-3
+                              py-1.5
+                              text-base
+                              font-normal
+                              text-gray-900
+                              bg-gray-100 bg-clip-padding
+                              border border-solid border-gray-300
+                              rounded
+                              transition
+                              ease-in-out
+                              m-0
+                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2"
+                              aria-describedby="emailHelp" placeholder="Pincode">
+                        </div>
+                        <button type="submit" class="
+                           w-full
+                           px-6
+                           py-2.5
+                           bg-customcolor
+                           text-white
+                           font-medium
+                           text-xs
+                           leading-tight
+                           uppercase
+                           rounded
+                           shadow-md
+                           hover:shadow-lg
+                           focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                           active:bg-blue-800 active:shadow-lg
+                           transition
+                           duration-150
+                           ease-in-out">Submit</button>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- section 2 -->
+     
+      <!-- End -->
+      <!-- Section 3 -->
+
+<div class="lg:h-screen flex items-center ">
+<div class="container mx-auto px-6 lg:px-32 ">
+     <h3 class="text-center text-3xl font-bold">Benefits</h3>
+     <p class="text-center text-lg pt-2">The benefits of installing a Drinkon24 water purification system include:
+     </p>
+<div class="grid items-center md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+            <div>
+               <ul class="list-reset text-black mb-8 pt-16  text-xl text-customcolor">
+                  <li class="flex gap-4 items-center mb-16 ">
+                     <img class="w-10 " src="assets/Fewer toxic icon.png" alt="Fewer toxic icon">
+                     <span>Fewer toxic chemicals in
+                        your tap water </span>
+                  </li>
+                  <li class="flex gap-4 items-center mb-16 ">
+                     <img class="w-10" src="assets/Better for icon.png" alt="Better for icon">
+                     <span>Better for the environment </span>
+                  </li>
+                  <li class="flex gap-4 items-center mb-16 ">
+                     <img class="w-10" src="assets/Cost-effective icon.png" alt="Cost-effective icon">
+                     <span>Cost-effective</span>
+                  </li>
+                  
+                  </ul>
+                  
+            </div>
+            <div>
+               <img src="assets/water glass.png" alt="water-galss">
+            </div>
+            <div>
+               <ul class="list-reset text-black mb-8 pt-16  text-xl text-customcolor">
+                  <li class="flex gap-4 items-center mb-16 ">
+                     <span>Improved quality of life </span>
+                     <img class="w-10 lg:ml-auto" src="assets/improve.png" alt="Fewer toxic icon">
+                     
+                  </li>
+                  <li class="flex gap-4 items-center mb-16 ">
+                     <span>Lower risk of certain 
+                        health problems </span>
+                     <img class="w-10 lg:ml-auto" src="assets/lower.png" alt="Better for icon">
+                     
+                  </li>
+                  <li class="flex gap-4 items-center mb-16 ">
+                     <span>Removes bacteria viruses & cysts .</span>
+                     <img class="w-10 lg:ml-auto" src="assets/remove.png" alt="Cost-effective icon">
+                    
+                  </li>
+                  
+                  </ul>
+            </div>
+
+
+
+
+               </div>
+               </div>
+               </div>
+
+      
+      <!-- End -->
+
+
+      <!-- section 3 -->
+      <div class="lg:h-screen flex items-center ">
+      <div class="container mx-auto px-6 lg:px-32 ">
+         <div class="grid lg:grid-cols-2 ">
+           <div class="pt-36 ">
+            <p class="text-customcolor font-bold text-2xl  ">Emergency Water Purification</p>
+            <h3 class="text-black text-5xl font-extrabold leading-none pt-4">Have a Water<br> Purification issue? We<br> can help!</h3>
+            <p class="text-gray text-xl pt-6">Vivamus non posuere nisl, id auctor ligula. Phasellus into<br>
+                vitae lobortis nisi, ut aliquam mauris. Crasy vitae erosers<br>
+                 metus. Suspendisse imperdiet massa metus, iacinia and<br> libero lobortis necauris feugiat.
+
+            </p>
+
+           </div>
+           <div class=" custom-3 py-32">
+           
+            
+               <div style="background-color: #f4fafe;" class="ml-auto w-full max-w-lg p-4 bg-white border rounded-lg sm:p-8">
+                  
+                  <!-- List -->
+                  <ul class="list-reset text-black mb-8   text-xl text-customcolor">
+                     <li class="flex gap-4 items-center mb-16 ">
+                        <img class="w-16 " src="assets/water-icon.png" alt="Fewer toxic icon">
+                        <div>
+                        <span class="text-customcolor text-lg font-medium">Same Day</span>
+                           <h3 class="text-black text-2xl font-bold pb-1">Emergency</h3>
+                           <p class="text-gray text-lg" >Vivamus non posuere nislid auctor ligula.<br> vitae lobortis nisi</p>
+                           </div>
+                     </li>
+                     <li class="flex gap-4 items-center mb-16 ">
+                        <img class="w-16" src="assets/water-icon.png" alt="Better for icon">
+                        <div>
+                           <span class="text-customcolor text-lg font-medium">Same Day</span>
+                              <h3 class="text-black text-2xl font-bold pb-1">Emergency</h3>
+                              <p class="text-gray text-lg" >Vivamus non posuere nislid auctor ligula.<br> vitae lobortis nisi</p>
+                              </div>
+                     </li>
+                     <li class="flex gap-4 items-center mb-16 ">
+                        <img class="w-16" src="assets/water-icon.png" alt="Cost-effective icon">
+                        <div>
+                           <span class="text-customcolor text-lg font-medium">Same Day</span>
+                              <h3 class="text-black text-2xl font-bold pb-1">Emergency</h3>
+                              <p class="text-gray text-lg" >Vivamus non posuere nislid auctor ligula.<br> vitae lobortis nisi</p>
+                              </div>
+                     </li>
+                     
+                     </ul>
+                  
+               </div>
+         
+           </div>
+         </div>
+      </div>
+      </div>
+      
+      <!-- End -->
+
+      <!-- section 4 -->
+      <div class="lg:h-screen flex items-center" style="background-color: #f4fafe;">
+         <div class="container mx-auto px-6 lg:px-32 ">
+            <div class="grid lg:grid-cols-2 gap-2 lg:gap-12">
+             <div>
+               <img src="assets/glass.png" alt="glass">
+             </div>
+             <div>
+               <div class=" pt-4 lg:pt-36 ">
+                  <p class="text-customcolor font-bold text-2xl  ">Emergency Water Purification</p>
+                  <h3 class="text-black text-5xl font-extrabold leading-none pt-4">Have a Water<br> Purification issue? We<br> can help!</h3>
+                  <p class="text-gray text-xl pt-6">Vivamus non posuere nisl, id auctor ligula. Phasellus into<br>
+                      vitae lobortis nisi, ut aliquam mauris. Crasy vitae erosers<br>
+                       metus. Suspendisse imperdiet massa metus, iacinia and<br> libero lobortis necauris feugiat.
+      
+                  </p>
+      
+                 </div>
+             </div>
+
+               </div>
+               </div>
+               </div>
+      <!-- End -->
+      <!-- section 5 -->
+      <div class="lg:h-screen flex items-center" style="background-color: #f4fafe;">
+         <div class="container mx-auto px-6 lg:px-32 ">
+            <h3 class="text-center text-5xl font-bold pb-20">Products</h3>
+    
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-12">
+
+<diV>
+   
+<div class="w-full py-10 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+   <a href="#">
+       <img class="px-8 mx-auto rounded-t-lg" src="assets/product-thumb1.png" alt="product image" />
+   </a>
+   <div class="px-5 pt-6">
+       <a href="#">
+           <h5 class="text-center text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Water Purifier 1</h5>
+       </a>
+
+       <div class="text-center pt-2">
+           <span class="text-lg text-gray-900 dark:text-white">$139.00</span>
+          </div>
+   </div>       
+</div>
+
+</diV>
+<div>
+   <div class="w-full py-10 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+          <img class="px-8 mx-auto rounded-t-lg" src="assets/product-thumb2.png" alt="product image" />
+      </a>
+      <div class="px-5 pt-6">
+          <a href="#">
+              <h5 class="text-center text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Water Purifier 1</h5>
+          </a>
+   
+          <div class="text-center pt-2">
+              <span class="text-lg text-gray-900 dark:text-white">$139.00</span>
+             </div>
+      </div>       
+   </div>
+</div>
+<div>
+   <div class="w-full py-10 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+          <img class="px-8 mx-auto rounded-t-lg" src="assets/product-thumb3.png" alt="product image" />
+      </a>
+      <div class="px-5 pt-6">
+          <a href="#">
+              <h5 class="text-center text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Water Purifier 1</h5>
+          </a>
+   
+          <div class="text-center pt-2">
+              <span class="text-lg text-gray-900 dark:text-white">$139.00</span>
+             </div>
+      </div>       
+   </div>
+</div>
+<div>
+   <div class="w-full py-10 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+          <img class="px-8 mx-auto rounded-t-lg" src="assets/product-thumb4.png" alt="product image" />
+      </a>
+      <div class="px-5 pt-6">
+          <a href="#">
+              <h5 class="text-center text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Water Purifier 1</h5>
+          </a>
+   
+          <div class="text-center pt-2">
+              <span class="text-lg text-gray-900 dark:text-white">$139.00</span>
+             </div>
+      </div>       
+   </div>
+</div>
+
+
+
+               </div>
+               </div>
+               </div>
+
+
+
+      <!-- End -->
+
+
+      <!-- section 6  -->
+      <div class="lg:h-screen flex items-center">
+         <div class="container mx-auto px-6 lg:px-32 ">
+            <div class="w-10/12 mx-auto mt-8 text-oscuro">
+  
+               <div class="flex flex-col justify-center w-full">
+                               <h3 class="mb-8">
+                                   <span class="text-3xl sm:text-4xl lg:text-5xl leading-titles font-bold w-full sm:w-6/12 lg:w-full">
+                                   Preguntas frecuentes
+                                  </span>
+                               </h3>
+                <div>
+                 <div class="relative mb-4">
+                   <input type="checkbox" id="toggle1" class="toggle hidden" />
+                   <label class="bg-gray-100 title block font-bold bg-white p-4 cursor-pointer" for="toggle1">
+                     Título 1
+                   </label>
+                   <div class="content bg-white overflow-hidden">
+                     <p class="p-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                   </div>
+                 </div>
+                 <div class="relative mb-4">
+                   <input type="checkbox" id="toggle2" class="toggle hidden" />
+                   <label class="bg-gray-100 title block font-bold bg-white p-4 cursor-pointer" for="toggle2">
+                     Título 2
+                   </label>
+                   <div class="content bg-white overflow-hidden">
+                     <p class="p-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                   </div>
+                 </div>
+                 <div class="relative mb-4">
+                   <input type="checkbox" id="toggle3" class="toggle hidden" />
+                   <label class="bg-gray-100 title block font-bold bg-white p-4 cursor-pointer" for="toggle3">
+                     Título 3
+                   </label>
+                   <div class="content bg-white overflow-hidden">
+                     <p class="p-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                   </div>
+                 </div>
+               </div>
+                 
+                 
+                 
+            </div>
+            </div>
+
+      <!-- End -->
+
+<?php get_footer() ?>
